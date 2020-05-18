7cf24c8718a3f8e723f6fbe469c7fae1634501b9
tankeeh <61459827+tankeeh@users.noreply.github.com>
2020-05-19 01:57:40 +0200

    finished PriorityQueue. have to start Menu'

--- zmytest/TreeUtils/TreeUtilsFunctions.cpp
+++ zmytest/TreeUtils/TreeUtilsFunctions.cpp
@@ -38,11 +38,43 @@ void InitialConcatStringForBinaryTree(lasd::BinaryTree<Data>& tree, void* par){
     tree.MapPreOrder(&InitialConcatString<Data>,par);
 }
 
+
+template <typename Data>
+void InitialConcatStringForBinaryTreeHeap(lasd::Heap<Data>& heap, void* par){
+    heap.MapPreOrder(&InitialConcatString<Data>,par);
+}
+
 template <typename Data>
 void FunIntEx4ForBinaryTreeHeap(lasd::Heap<Data>& heap){
     heap.MapPreOrder(&MapFunIntEx4<Data>, nullptr);
 }
 
+template <typename Data>
+void NegatifyDataForBinaryTreeHeap(lasd::Heap<Data>& heap){
+    heap.MapPreOrder(&NegatifyData<Data>, nullptr);
+}
+
+
+template <typename Data>
+Data FoldTreeHeapIntMoltiplicateSmallerThan(const Data& par, lasd::Heap<Data> &heap){
+    int acc = 1;
+    heap.FoldPreOrder(&MoltiplicateInt<Data>,&par,&acc);
+    return acc;
+}
+
+template <typename Data>
+Data FoldTreeHeapFloatSumBiggerThan(const Data& par, lasd::Heap<Data> &heap){
+    float acc = 0;
+    heap.FoldPreOrder(&SumFloatBiggerThan<Data>,&par,&acc);
+    return acc;
+}
+
+template <typename Data>
+Data FoldTreeHeapStringConcatLowerEqualsThan(const int& par, lasd::Heap<Data> &heap){
+    std::string acc = "";
+    heap.FoldPreOrder(&StringConcat<Data>,&par,&acc);
+    return acc;
+}
 
 template <typename Data>
 Data FoldTreeIntMoltiplicateSmallerThan(const Data& par, lasd::BinaryTree<Data> &tree){
@@ -50,6 +82,8 @@ Data FoldTreeIntMoltiplicateSmallerThan(const Data& par, lasd::BinaryTree<Data> 
     tree.FoldPreOrder(&MoltiplicateInt<Data>,&par,&acc);
     return acc;
 }
+
+
 
 template <typename Data>
 Data FoldTreeStringConcatLowerEqualsThan(const int& par, lasd::BinaryTree<Data> &tree){
--- zmytest/TreeUtils/TreeUtilsFunctions.hpp
+++ zmytest/TreeUtils/TreeUtilsFunctions.hpp
@@ -23,6 +23,15 @@ template <typename Data>
 void InitialConcatStringForBinaryTree(lasd::BinaryTree<Data>& tree, void* par);
 
 template <typename Data>
+void InitialConcatStringForBinaryTreeHeap(lasd::BinaryTree<Data>& tree, void* par);
+
+template <typename Data>
+void FunIntEx4ForBinaryTreeHeap(lasd::Heap<Data>& heap);
+
+template <typename Data>
+void NegatifyDataForBinaryTreeHeap(lasd::Heap<Data>& heap);
+
+template <typename Data>
 void PrintElementTreeBreadth(lasd::BinaryTree<Data>& tree);
 
 template <typename Data>
@@ -30,6 +39,15 @@ void PrintElementTreeInOrder(lasd::BinaryTree<Data>& tree);
 
 template <typename Data>
 void PrintElementTreePostOrder(lasd::BinaryTree<Data>& tree);
+
+template <typename Data>
+Data FoldTreeHeapIntMoltiplicateSmallerThan(const Data& par, lasd::Heap<Data> &heap);
+
+template <typename Data>
+Data FoldTreeHeapFloatSumBiggerThan(const Data& par, lasd::Heap<Data> &heap);
+
+template <typename Data>
+Data FoldTreeHeapStringConcatLowerEqualsThan(const int& par, lasd::Heap<Data> &heap);
 
 template <typename Data>
 Data FoldTreeIntMoltiplicateSmallerThan(const Data& par, lasd::BinaryTree<Data> &tree);
