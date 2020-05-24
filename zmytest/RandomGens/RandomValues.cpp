33bc4b7d628e3df75df3c76cd51abeefb1bc341c
tankeeh <61459827+tankeeh@users.noreply.github.com>
2020-05-24 21:30:10 +0200

    finished

--- zmytest/RandomGens/RandomValues.cpp
+++ zmytest/RandomGens/RandomValues.cpp
@@ -5,14 +5,21 @@
 #include <random>
 #include "RandomValues.hpp"
 
+
+
+std::default_random_engine RandGen(std::random_device{}());
+
+int UnsignedIntValueGenerator(){
+    std::uniform_int_distribution<> RandIntDistribution(1, 20);
+    return (RandIntDistribution(RandGen));
+}
+
 int IntValueGenerator(){
-    std::default_random_engine RandGen(std::random_device{}());
     std::uniform_int_distribution<> RandIntDistribution(-500, 500);
     return (RandIntDistribution(RandGen));
 }
 
 float FloatValueGenerator(){
-    std::default_random_engine RandGen(std::random_device{}());
     std::uniform_real_distribution<> RandFloatDistribution(-500, 500);
     float temp = (RandFloatDistribution(RandGen));
     temp = round(temp*1000)/1000;
@@ -21,8 +28,7 @@ float FloatValueGenerator(){
 
 std::string CharValueGenerator(){
     std::string returnstring = "";
-    std::default_random_engine RandGen(std::random_device{}());
     std::uniform_int_distribution<> RandCharDistribution(33,126);
-    for(int i=0; i<5; i++) returnstring += (RandCharDistribution(RandGen));
+    for(int i=0; i<UnsignedIntValueGenerator(); i++) returnstring += (RandCharDistribution(RandGen));
     return returnstring;
 }
